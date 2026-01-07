3f3adfb38c05d3d31f433baabebc917213ddec87
Robert Sutton <150315871+RSutton16@users.noreply.github.com>
2026-01-06 20:40:33 -0600

    edited index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <h1>Home</h1>
-    <v-btn prepend-icon="mdi-emoticon-happy">Make home life Easy</v-btn>
+    <v-btn prepend-icon="mdi-emoticon-happy">Home Page!</v-btn>
   </v-container>
 </template>
 
